54a14c51438bbc78f1dde9ab0e74d84c3f9860d3
harsha <harsha.b003@gmail.com>
2020-05-09 04:03:50 -0400

    Initial Commit

---
+++ labPlace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  labPlace
+//
+//  Created by Venkata harsha Balla on 5/9/20.
+//  Copyright © 2020 BVH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
